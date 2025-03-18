4630486e0b870940beeac86c8ab247b998ab4cc9
won <jwon3991.9191@gmail.com>
2025-03-18 17:43:39 +0900

    first commit

---
+++ src/main/kotlin/ecommerce/server/ECommerceApplication.kt
@@ -0,0 +1,11 @@
+package ecommerce.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ECommerceApplication>(*args)
+}
